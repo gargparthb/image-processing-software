74638e9445c62925bfecf742a9853c83b7b697ff
gargparthb <62770333+gargparthb@users.noreply.github.com>
2021-01-22 10:44:40 -0500

    image editor class

---
+++ src/main/java/io/github/gargparthb/ImageEditor.java
@@ -0,0 +1,12 @@
+package io.github.gargparthb;
+
+import java.io.File;
+
+public class ImageEditor {
+  File img;
+
+  public ImageEditor(File img) {
+    this.img = img;
+  }
+
+}
--- src/main/java/io/github/gargparthb/Main.java
+++ src/main/java/io/github/gargparthb/Main.java
@@ -1,18 +1,25 @@
 package io.github.gargparthb;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.lang.reflect.Method;
 
 public class Main {
   public static void main(String[] args) {
-    Method exec = CommandLine.getCommandMethods(Main.class, "run").get(0);
-    CommandLine cmd = new CommandLine(exec);
+    Method run = CommandLine.getCommandMethods(Main.class, "run").get(0);
+    CommandLine cmd = new CommandLine(run);
     cmd.execute(args);
   }
 
-  @CommandLine.Command(description = "")
-  public void run(@CommandLine.Option(names = "-m") String msg) {
-    System.out.println(msg);
+  @Command(name = "edit",
+          description = "edit the given image(s)",
+          version = "1.0.0",
+          mixinStandardHelpOptions = true)
+  public void run(@Parameters(paramLabel = "main image") File img) {
+
   }
 }
