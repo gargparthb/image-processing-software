d138a01e24195f72b1e245a1300184061730f7ba
Parth Garg <gargparthb@gmail.com>
2021-01-22 11:25:15 -0500

    main IO functions

--- src/main/java/io/github/gargparthb/ImageEditor.java
+++ src/main/java/io/github/gargparthb/ImageEditor.java
@@ -1,12 +1,21 @@
 package io.github.gargparthb;
 
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+
 import java.io.File;
+import java.io.IOException;
 
 public class ImageEditor {
-  File img;
+  BufferedImage img;
 
-  public ImageEditor(File img) {
-    this.img = img;
+  ImageEditor(File img) throws IOException {
+    this.img = ImageIO.read(img);
   }
 
+  // main editing method
+  void edit() {
+
+  }
 }
--- src/main/java/io/github/gargparthb/Main.java
+++ src/main/java/io/github/gargparthb/Main.java
@@ -4,22 +4,34 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Help.Ansi;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 
 public class Main {
   public static void main(String[] args) {
+    // points cmd args to the run method
     Method run = CommandLine.getCommandMethods(Main.class, "run").get(0);
     CommandLine cmd = new CommandLine(run);
     cmd.execute(args);
   }
 
+  // starts the ImageEditor class
   @Command(name = "edit",
           description = "edit the given image(s)",
           version = "1.0.0",
           mixinStandardHelpOptions = true)
   public void run(@Parameters(paramLabel = "main image") File img) {
+    try {
+      // starts the editor
+      ImageEditor editor = new ImageEditor(img);
+      editor.edit();
 
+      System.out.println(Ansi.AUTO.string("@|bold,green Image Processed!|@"));
+    } catch (IOException e) {
+      System.out.println(Ansi.AUTO.string("@|bold,red fatal: Invalid Path|@"));
+    }
   }
 }
